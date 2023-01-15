26db9f6388dfc041fabebe3dc275bcb8650a1e3f
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-01-15 19:45:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string korisnik = Console.ReadLine();
+            string[] polje = korisnik.Split(' ');
+            int cijeli = Convert.ToInt32(polje[0])* 2;
+            double decimalni = Convert.ToDouble(polje[1])* 2;
+            Console.WriteLine(cijeli + " " + decimalni);
+
+            Console.ReadKey();
+        }
+    }
+}
